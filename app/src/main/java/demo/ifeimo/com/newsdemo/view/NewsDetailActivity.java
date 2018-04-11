15efc4568730ab301daf1e346ea1733a6e358240
dengweiyu <1030959243@qq.com>
2018-04-11 23:12:44 +0800

    doing news_detail

--- app/src/main/java/demo/ifeimo/com/newsdemo/view/NewsDetailActivity.java
+++ app/src/main/java/demo/ifeimo/com/newsdemo/view/NewsDetailActivity.java
@@ -1,9 +1,18 @@
 package demo.ifeimo.com.newsdemo.view;
 
+import android.os.Bundle;
+import demo.ifeimo.com.newsdemo.R;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Wing on 2018/4/11.
  */
 
-public class NewsDetailActivity {
+public class NewsDetailActivity extends AppCompatActivity{
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_detail);
+    }
 }
